42fbe04613e535f71c3d8b4c0c8a37eb7f9214ef
Matt Webb <matt@interconnected.org>
2023-09-01 08:49:39 +0100

    Full page YouTube

--- src/app/Jukebox.tsx
+++ src/app/Jukebox.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import usePartySocket from "partysocket/react";
 import ReactPlayer from "react-player";
 import ConnectionStatus from "@/app/components/ConnectionStatus";
@@ -15,6 +15,12 @@ export default function Jukebox() {
   const [player, setPlayer] = useState<ReactPlayer | null>(null);
   const [showPlayer, setShowPlayer] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: 1600,
+    height: 900,
+  });
+  const [playerIsVertical, setPlayerIsVertical] = useState(false);
+  const ASPECT_RATIO = 16 / 9;
 
   const socket = usePartySocket({
     host: host,
@@ -27,6 +33,24 @@ export default function Jukebox() {
       }
     },
   });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    const isVertical =
+      windowDimensions.width / windowDimensions.height < ASPECT_RATIO;
+    setPlayerIsVertical(isVertical);
+  }, [windowDimensions]);
 
   useEffect(() => {
     setShowPlayer(true);
@@ -52,18 +76,44 @@ export default function Jukebox() {
     socket.send(JSON.stringify({ type: "playback", state: "pause" }));
   };
 
+  // Namibia https://www.youtube.com/watch?v=ydYDqZQpim8
+  // Lofi beats https://www.youtube.com/watch?v=jfKfPfyJRdk
+
+  const playerDimensions = {
+    width: playerIsVertical
+      ? windowDimensions.height * ASPECT_RATIO
+      : windowDimensions.width,
+    height: playerIsVertical
+      ? windowDimensions.height
+      : windowDimensions.width / ASPECT_RATIO,
+  };
+
   return (
     <div>
       <ConnectionStatus socket={socket} />
       {showPlayer && (
-        <ReactPlayer
-          url="https://www.youtube.com/watch?v=ydYDqZQpim8"
-          muted
-          playing={isPlaying}
-          onReady={(event) => onReady(event)}
-          onPlay={() => onPlay()}
-          onPause={() => onPause()}
-        />
+        <div
+          className="fixed top-0"
+          style={{
+            marginTop: playerIsVertical
+              ? "0"
+              : `-${(playerDimensions.height - windowDimensions.height) / 2}px`,
+            marginLeft: `-${playerDimensions.width / 2}px`,
+          }}
+        >
+          <div className="absolute top-0 left-0 right-0 bottom-0 w-screen h-screen">
+            <ReactPlayer
+              url="https://www.youtube.com/watch?v=ydYDqZQpim8"
+              muted
+              playing={isPlaying}
+              onReady={(event) => onReady(event)}
+              onPlay={() => onPlay()}
+              onPause={() => onPause()}
+              width={`${playerDimensions.width}px`}
+              height={`${playerDimensions.height}px`}
+            />
+          </div>
+        </div>
       )}
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Jukebox from "./Jukebox";
 export default function Home() {
   return (
     <main
-      className="flex min-h-screen flex-col items-center justify-start p-6"
+      className="relative flex min-h-screen flex-col items-center justify-start p-6"
       style={{ minHeight: "100dvh" }}
     >
       <h1 className="text-4xl font-semibold pb-6">YouTube party</h1>
