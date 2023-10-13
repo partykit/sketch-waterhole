b9b24a8fa38ab520507794a4df0461611add740e
Matt Webb <matt@interconnected.org>
2023-10-13 08:54:01 +0100

    Small screen tweak

--- app/components/video-credit.tsx
+++ app/components/video-credit.tsx
@@ -2,8 +2,9 @@ export default function VideoCredit() {
   return (
     <div className="absolute top-0 right-0 text-white/60 font-mono text-xs bg-black px-1">
       <a href="https://www.youtube.com/watch?v=ydYDqZQpim8">
-        Live stream of the Namib Desert by NamibiaCam.{" "}
-        <span className="underline">YouTube</span>
+        Live stream{" "}
+        <span className="hidden sm:inline">of the Namib Desert </span>by
+        NamibiaCam. <span className="underline">YouTube</span>
       </a>
     </div>
   );
